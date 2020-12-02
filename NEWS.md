6165a08f726628afd491eed4a26893baef353f80
C. Lente <7017340+clente@users.noreply.github.com>
2020-12-02 20:46:39 -0300

    Python-chess needs Python 3.8 (#3)

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,4 @@
 # chess 1.0.0.9000
 
+* `install_chess()` must use Python 3.8.
 * Added a `NEWS.md` file to track changes to the package.
--- R/python.R
+++ R/python.R
@@ -1,11 +1,19 @@
 
 #' Install python-chess
+#'
+#' Install the python library used as the backbone of this package. You can
+#' pass arguments on to [reticulate::py_install()], but `python-chess` needs
+#' `python_version = "3.8"` and `pip = TRUE`.
+#'
 #' @param method Installation method
 #' @param conda The path to a conda executable
 #' @param ... Other arguments passed on to [reticulate::py_install()]
 #' @export
 install_chess <- function(method = "auto", conda = "auto", ...) {
-  reticulate::py_install("chess", method = method, conda = conda, ...)
+  reticulate::py_install(
+    "chess", method = method, conda = conda,
+    python_version = "3.8", pip = TRUE, ...
+  )
 }
 
 # Environment for globals
