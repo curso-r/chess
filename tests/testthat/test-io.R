ff68d5ad427c69614b72b449dcaa3b94ee7d0759
C. Lente <7017340+clente@users.noreply.github.com>
2020-12-04 14:52:13 -0300

    Plotting test must happen only on Linux

--- tests/testthat/test-io.R
+++ tests/testthat/test-io.R
@@ -43,3 +43,32 @@ test_that("reading and writing PGN works", {
   dev.off()
   expect_snapshot_file(tmp, "immortal.png")
 })
+
+test_that("plotting works", {
+
+  # Skip if python-chess is not available
+  if (!reticulate::py_module_available("chess")) {
+    skip("python-chess not available for testing")
+  }
+
+  # Skip if not on Linux (resolutions break snapshot)
+  skip_on_os(c("windows", "mac"))
+
+  # Immortal game
+  standard <- game() %>%
+    move(
+      "e4", "e5", "f4", "exf4", "Bc4", "Qh4+", "Kf1", "b5", "Bxb5", "Nf6", "Nf3",
+      "Qh6", "d3", "Nh5", "Nh4", "Qg5", "Nf5", "c6", "g4", "Nf6", "Rg1", "cxb5",
+      "h4", "Qg6", "h5", "Qg5", "Qf3", "Ng8", "Bxf4", "Qf6", "Nc3", "Bc5", "Nd5",
+      "Qxb2", "Bd6", "Bxg1? {It is from this move that Black's defeat stems.}",
+      list("Qxa1+ {Wilhelm Steinitz suggested in 1879...}", "Ke2", "Qb2", "Kd2", "Bxg1"),
+      "e5", "Qxa1+", "Ke2", "Na6", "Nxg7+", "Kd8", "Qf6+", "Nxf6", "Be7#"
+    )
+
+  # Plot
+  tmp <- tempfile(fileext = ".png")
+  png(filename = tmp)
+  plot(standard)
+  dev.off()
+  expect_snapshot_file(tmp, "immortal.png")
+})
